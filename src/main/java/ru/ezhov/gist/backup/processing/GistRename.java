9804fd111f6e06ede3980815918444aaf06dd9f6
ezhov-da <ezhdenis@yandex.ru>
2019-03-08 11:40:33 +0300

    Добавил переименование GIST

--- src/main/java/ru/ezhov/gist/backup/processing/GistRename.java
+++ src/main/java/ru/ezhov/gist/backup/processing/GistRename.java
@@ -7,9 +7,15 @@ import org.eclipse.egit.github.core.service.GistService;
 import ru.ezhov.gist.backup.GistBackup;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -34,26 +40,57 @@ public class GistRename {
             System.out.println("Имя пользователя '" + username + "'");
         }
 
+        Map<String, String> map = getMapOldNewNames();
+
         try {
             GitHubClient gitHubClient = new GitHubClient();
             gitHubClient.setOAuth2Token(gistToken);
             GistService gistService = new GistService(gitHubClient);
-            List<Gist> gistList = null;
-            gistList = gistService.getGists(username);
+            List<Gist> gistList = gistService.getGists(username);
             for (Gist gist : gistList) {
                 Map<String, GistFile> fileMap = gist.getFiles();
                 for (Map.Entry<String, GistFile> fileEntry : fileMap.entrySet()) {
-                    String name = fileEntry.getKey();
-                    GistFile gistFile = fileEntry.getValue();
-                    if ("test-file".equals(name)) {
-                        gistFile.setFilename("test-file-from-java");
-                        gistService.updateGist(gist);
-                        break;
+                    String oldFileName = fileEntry.getKey();
+                    String newName = map.get(oldFileName);
+                    if (newName == null) {
+                        System.out.println("Новое имя для GIST '" + oldFileName + "' не найдено.");
+                    } else {
+                        updateGistFileName(gist.getUrl(), gistToken, oldFileName, newName);
+                        System.out.println("Обновлено название файла с '" + oldFileName + "' на '" + newName + "'");
                     }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void updateGistFileName(String gistUrl, String token, String oldName, String newName) throws Exception {
+        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(gistUrl).openConnection();
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Authorization", "token " + token);
+        httpURLConnection.setDoOutput(true);
+        String updateFilename = "{\"files\": {\"" + oldName + "\": {\"filename\": \"" + newName + "\"}}}";
+        System.out.println("send -> " + updateFilename);
+        try (OutputStream outputStream = httpURLConnection.getOutputStream()) {
+            outputStream.write(updateFilename.getBytes(StandardCharsets.UTF_8));
+        }
+        int code = httpURLConnection.getResponseCode();
+        if (code != HttpURLConnection.HTTP_OK) {
+            throw new Exception("Ошибка обновления GIST: " + oldName + " ->  " + newName);
+        }
+        httpURLConnection.disconnect();
+    }
+
+    private static Map<String, String> getMapOldNewNames() {
+        Map<String, String> map = new HashMap<>();
+        try (Scanner scanner = new Scanner(GistRename.class.getResourceAsStream("/out-1551994528156.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] array = line.split("\\|\\|");
+                map.put(array[0], array[1]);
+            }
+        }
+        return map;
     }
 }
